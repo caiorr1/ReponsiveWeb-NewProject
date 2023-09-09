02d3cdb320e8ba4a8739e0abcc1ea45f00f974a3
bielcosta6905@gmail.com <bielcosta6905@gmail.com>
2023-09-09 20:27:53 -0300

    estilização do header e moldura do bot

---
+++ src/app/components/HeaderChat/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const BotImage = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-right: 10px;
+`;
+const Header = styled.header`
+  background-color: #333;
+  color: black;
+  padding: 1rem;
+`;
--- src/app/prototipo/styled.js
+++ src/app/prototipo/styled.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+const HeaderWrapper = styled.header`
+  background-color: #333;
+  color: white;
+  padding: 1rem;
+`;
+
+
+
 const HeaderBot = styled.Bot`
 
 
