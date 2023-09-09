1c0bd63af82e67945d4b794e38362eac3ed0a3f4
projetosfiap <rm99759@fiap.com.br>
2023-09-09 01:00:21 -0300

    feat: arrumando moldura

--- src/app/components/MobileScreen/mobilescreen.js
+++ src/app/components/MobileScreen/mobilescreen.js
@@ -1,11 +1,13 @@
-
 import React from 'react';
+import Header from '../HeaderChat/headerchat'
 import MobileScreenSimulation from './styled'; 
 
 export default function MobileScreen(){
   return (
     <div>
-      <MobileScreenSimulation></MobileScreenSimulation>
+      <MobileScreenSimulation>
+        <Header></Header>
+      </MobileScreenSimulation>
     </div>
   );
 };
--- src/app/components/MobileScreen/styled.js
+++ src/app/components/MobileScreen/styled.js
@@ -3,15 +3,16 @@
 import styled from 'styled-components';
 
 const MobileScreenSimulation = styled.div`
-  width: 375px; 
-  height: 812px; 
-  border: 2px solid #000; 
-  border-radius: 10px; 
-  margin: 56px auto 0; 
-  padding: 0; 
-  overflow: hidden; 
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
-  background-color: #fff; 
+  width: 80vw; /* Largura em relação à largura da viewport */
+  height: 90vh; /* Altura em relação à altura da viewport */
+  max-width: 588px; /* Largura máxima para limitar o tamanho em telas maiores */
+  border: 2px solid #000;
+  border-radius: 10px;
+  margin: 56px auto 0;
+  padding: 0;
+  overflow: hidden;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  background-color: #fff;
 `;
 
 export default MobileScreenSimulation;
