c4d82bcff5e234471e7fae8d5e2cfd9c50c605ee
caiorr1 <caiorrodrigues2004@gmail.com>
2023-09-12 22:01:51 -0300

    refactor: alterando estrutura

--- src/app/components/Footer/footer.js
+++ src/app/components/Footer/footer.js
@@ -2,10 +2,12 @@ import { FooterWrapper, Text } from './styled';
 
 export default function RodapePadrao() {
     return (
-      <FooterWrapper>
-        <Text>AndromedaSys® copyright 2023.</Text>
-        <Text>Todos os direitos reservados.</Text>
-      </FooterWrapper>
+      <footer>
+        <FooterWrapper>
+          <Text>AndromedaSys® copyright 2023.</Text>
+          <Text>Todos os direitos reservados.</Text>
+        </FooterWrapper>
+      </footer>
     );
   }
   
--- src/app/components/PageBody/pagebody.js
+++ src/app/components/PageBody/pagebody.js
@@ -2,17 +2,15 @@ import { PageBodyWrapper, ContentWrapper, Title, Description1, Description2, Log
 import BotaoConhecaPlanos from "../Button/button";
 
 export default function BodyPadrao() {
-    return (
+  return (
       <PageBodyWrapper>
-        <ContentWrapper>
+      <ContentWrapper>
         <Title>AndromedaSys</Title>
-        <Description1>{"A solução da sua dor você a encontra aqui!"}</Description1>
-        <br />
-        <br />
-        <Description2>{"Faça um orçamento sem compromisso!"}</Description2>
+        <Description1>A solução da sua dor você encontra aqui!</Description1>
+        <Description2>Faça um orçamento sem compromisso!</Description2>
         <BotaoConhecaPlanos text="CONHECER PLANO" />
-        </ContentWrapper>
-        <Logo src={"../images/LogoNome-Andromeda.png"} alt="LogoAndromeda" />
-      </PageBodyWrapper>
-    );
-  };  
+      </ContentWrapper>
+      <Logo src="../images/LogoNome-Andromeda.png" alt="LogoAndromeda" />
+    </PageBodyWrapper>
+  );
+};  
